4acd804a1d1d4d144d8b36b422540e7d9d36a67d
Yuuta Sumioka <ysumio120@yahoo.com>
2016-11-09 19:23:09 -0500

    Implemented JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	host: "127.0.0.1",
-	port: 3306,
-	user: "root",
-	password: "password",
-	database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+	connection = mysql.createConnection({
+		host: "127.0.0.1",
+		port: 3306,
+		user: "root",
+		password: "password",
+		database: "burgers_db"
+	});
+}
 
 connection.connect(function (err) {
 	if (err) {
